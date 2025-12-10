84ea5e5f000cd34d42cf237fd87ef438f9da6b0f
Murtaza Amin <murtazaamin1@gmail.com>
2025-12-10 16:24:34 -0700

    feat: Update agent's gamma hyperparameter from 0.7 to 0.85.

--- agent.py
+++ agent.py
@@ -20,7 +20,7 @@ class MonteCarloAgent:
         
         # Hyperparameters
         self.batch_size = 5000 # Batch size for training (doubled with mirroring)
-        self.gamma = 0.7 #(discount factor for Monte Carlo return, prioritizes most recent moves)
+        self.gamma = 0.85 #(discount factor for Monte Carlo return, prioritizes most recent moves)
         self.epsilon = 1.00 # Initial exploration rate
         self.epsilon_min = self.params.get('epsilon_min_percent', 5) / 100.0 # Minimum exploration rate (Default 5%)
         self.epsilon_decay = 0.9997 # Decay per training step
